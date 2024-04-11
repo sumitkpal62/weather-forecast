adde70d2afdcc294505f8e2467bcec8a3c016f62
sumitkpal62 <sciencebuddy51@gmail.com>
2024-04-12 04:18:26 +0530

    header component added

---
+++ app/components/Header.tsx
@@ -0,0 +1,29 @@
+'use client'
+import Link from "next/link";
+import { useState } from "react";
+import { MdLightMode } from "react-icons/md";
+
+
+export default function Header() {
+  const [active, setActive] = useState(false);
+
+  return (
+    <nav className="flex bg-[#333] justify-items-center items-center justify-between text-white">
+      <div className="p-2 mx-4 my-2 w-1/2">
+        <h1 className="text-3xl font-bold">Weather Forcast</h1>
+      </div>
+      <ul className="flex gap-6 font-semibold text-xl p-2 w-1/4 justify-center">
+        <li >
+          <Link href={'/'}>Home</Link>
+        </li>
+        <li>
+          <Link href="/weather">Weather</Link>
+        </li>
+      </ul>
+      <button className="flex justify-center gap-1 w-1/4">
+        Light
+        <MdLightMode />
+      </button>
+    </nav>
+  );
+}
---
+++ app/weather/page.tsx
@@ -0,0 +1,9 @@
+
+
+const Weather = () => {
+  return (
+    <h1 className={'text-6xl'}>Weather Page</h1>
+  )
+}
+
+export default Weather;
